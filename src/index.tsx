a5ecf9e06a9585150b1204668d58ca448b1edc8b
eabinn <iby2455@gmail.com>
2022-05-13 17:48:34 +0900

    Config mui theme and AuthProvider

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,40 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./components/app";
+import { AuthProvider } from "./contexts/AuthProvider";
 import reportWebVitals from "./reportWebVitals";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+import "./assets/css/reset.css";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#0053F4",
+    },
+    error: {
+      main: "#DB3645",
+    },
+    info: {
+      main: "#142C67",
+    },
+    text: {
+      disabled: "#5F6E8C",
+      primary: "#131924",
+      secondary: "#5F6E8C",
+    },
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ThemeProvider theme={theme}>
+    <React.StrictMode>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </React.StrictMode>
+  </ThemeProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
