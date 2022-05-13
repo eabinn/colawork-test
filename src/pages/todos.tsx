5eaeb6c53ce42123dcf7dac46177e1cc0a1159f4
eabinn <iby2455@gmail.com>
2022-05-13 17:05:36 +0900

    Create todos page

---
+++ src/pages/todos.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Container, Box } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { useTodoState } from "../contexts/TodoProvider";
+import { TodoBox } from "../components/TodoBox";
+
+export const Todos: React.FC = () => {
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.up("md"));
+  const todoState = useTodoState();
+
+  return (
+    <Box
+      sx={{
+        minHeight: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        padding: matches ? 0 : "80px 0",
+      }}
+    >
+      <Container>
+        <Box sx={{ display: "flex", justifyContent: "space-evenly", flexDirection: matches ? "row" : "column", gap: matches ? 0 : "30px" }}>
+          <TodoBox todoType="notDone" todoState={todoState} />
+          <TodoBox todoType="done" todoState={todoState} />
+        </Box>
+      </Container>
+    </Box>
+  );
+};
