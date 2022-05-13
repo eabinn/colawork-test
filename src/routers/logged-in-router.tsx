a7307a79513d8b109c708a1a850f1fdffdcffdea
eabinn <iby2455@gmail.com>
2022-05-13 16:59:37 +0900

    Create a router with routes for logged in user

---
+++ src/routers/logged-in-router.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { BrowserRouter, Routes, Route, PathRouteProps } from "react-router-dom";
+import { Header } from "../components/header";
+import { TodoProvider } from "../contexts/TodoProvider";
+import { NotFound } from "../pages/NotFound";
+import { Todos } from "../pages/todos";
+
+const routes: PathRouteProps[] = [
+  {
+    path: "/list",
+    element: <Todos />,
+  },
+];
+
+export const LoggedInRouter: React.FC = () => {
+  return (
+    <BrowserRouter>
+      <Header />
+      <TodoProvider>
+        <Routes>
+          <Route path="*" element={<NotFound />} />
+          {routes.map((route) => (
+            <Route path={route.path} element={route.element} key={route.path} />
+          ))}
+        </Routes>
+      </TodoProvider>
+    </BrowserRouter>
+  );
+};
